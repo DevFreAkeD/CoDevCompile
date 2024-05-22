2d8938db6b0e71948257a84eac8cab63becc3542
Tushar Nagar <tusharrock146@gmail.com>
2024-05-22 21:14:08 +0530

    Update Sidebar.jsx

--- src/componenets/Sidebar.jsx
+++ src/componenets/Sidebar.jsx
@@ -5,7 +5,12 @@ const Sidebar = () => {
     <div className="h-screen w-16 flex flex-col bg-gray-800 text-white shadow-lg">
       <div className="flex flex-col items-center mt-4">
         {icons.map(item => (
-          <button key={item.id} className="p-3 mt-2 hover:bg-gray-700 rounded-lg" aria-label={item.title}>
+          <button
+            key={item.id}
+            className="p-3 mt-2 hover:bg-gray-700 rounded-lg"
+            aria-label={item.title}
+            onClick={() => onLanguageChange(item.language)}
+          >
             <img src={item.icon} alt={item.title} width={item.width} height={item.height} />
           </button>
         ))}
