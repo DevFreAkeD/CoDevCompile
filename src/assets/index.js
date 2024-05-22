a74d8978bcde9f5b2b45514b09a36a4eba41c3c1
Tushar Nagar <tusharrock146@gmail.com>
2024-05-22 12:47:43 +0530

    Initial Commit

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import clang from "./language-icons/c.svg"
+import cpplang from "./language-icons/cpp.svg"
+import jslang from "./language-icons/js.svg"
+import java from "./language-icons/java.svg"
+import Csharp from "./language-icons/csharp.svg"
+import python from "./language-icons/python.svg"
+import logo from "./icon.png"
+
+export {
+    clang,
+    cpplang,
+    jslang,
+    Csharp,
+    python,
+    java,
+    logo,
+};
