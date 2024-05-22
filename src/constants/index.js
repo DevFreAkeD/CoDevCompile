7a68dcea54a54dbe870e141bd05fd677da80c3e8
Tushar Nagar <tusharrock146@gmail.com>
2024-05-22 12:47:29 +0530

    Initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,53 @@
+import {
+    clang,
+    cpplang,
+    jslang,
+    Csharp,
+    java,
+    python,
+} from "../assets"
+
+export const icons = [
+    {
+      id: "0",
+      title: "C++",
+      icon: clang,
+      width: 34,
+      height: 35,
+    },
+    {
+      id: "1",
+      title: "C++",
+      icon: cpplang,
+      width: 34,
+      height: 34,
+    },
+    {
+        id: "2",
+        title: "JavaScript",
+        icon: jslang,
+        width: 34,
+        height: 34,
+    },
+    {
+        id: "3",
+        title: "Java",
+        icon: java,
+        width: 34,
+        height: 34,
+    },
+    {
+        id: "4",
+        title: "Python",
+        icon: python,
+        width: 34,
+        height: 34,
+    },
+    {
+        id: "5",
+        title: "C Sharp",
+        icon: Csharp,
+        width: 34,
+        height: 34,
+    },
+];
