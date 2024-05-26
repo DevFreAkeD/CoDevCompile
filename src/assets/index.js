d1d0f7368038bb6b7f8e40737949ba089d25e4cf
Tushar Nagar <tusharrock146@gmail.com>
2024-05-26 17:22:08 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,9 @@ import java from "./language-icons/java.svg"
 import Csharp from "./language-icons/csharp.svg"
 import python from "./language-icons/python.svg"
 import logo from "./icon.png"
+import githubicon from "./icons/github.png"
+import author from "./icons/author.png"
+import issue from "./icons/issue.png"
 
 export {
     clang,
@@ -14,4 +17,7 @@ export {
     python,
     java,
     logo,
+    githubicon,
+    author,
+    issue,
 };
